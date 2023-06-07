a02775636ffc54012cac8a87c43b096b1286528e
Kevinazo <kevinsama.moreno@gmail.com>
2023-06-07 00:26:24 -0700

    Integrando funcion de encriptado y desencriptado con JS

--- Encriptador-alura/main.js
+++ Encriptador-alura/main.js
@@ -1,4 +1,48 @@
+const textArea = document.querySelector(".area-texto")
+const mensaje = document.querySelector(".mensaje")
+/*Uso de arreglos multidimensionales, es decir arreglos de arreglo
+*/
 
-const areaTexto = document.getElementsByClassName(area-texto)
-const areaDesencriptado = document.getElementsByClassName(area-desencriptado)
+/*
+La letra e es convertida para "enter"
+La letra i es convertida para "imes"
+La letra a es convertida para "ai"
+La letra o es convertida para "ober"
+La letra u es convertida para "ufat"*/
+
+function btnEncriptar (){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+}
+
+function encriptar (stringEncriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
+function btnDesencriptado (){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+}
+
+function desencriptar (stringEncriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(i = 1; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][1])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringEncriptado
+}
+
+
 
